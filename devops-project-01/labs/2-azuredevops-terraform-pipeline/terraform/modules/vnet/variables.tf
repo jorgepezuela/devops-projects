26542f952fc500049448a7edd8d11f02d32fbc9a
jpezuela <jorge.pezuela@hotmail.com>
2025-05-10 17:18:24 +0200

    feature: add aks/log-analytics/vnet terraform modules

---
+++ devops-project-01/labs/2-azuredevops-terraform-pipeline/terraform/modules/vnet/variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+}
+
+variable "location" {
+  default = "westeurope"
+}
+
+variable "network_address_space" {
+}
+
+variable "aks_subnet_address_prefix" {
+}
+
+variable "aks_subnet_address_name" {
+}
+
+variable "appgw_subnet_address_prefix" {
+}
+
+variable "appgw_subnet_address_name" {
+}
+
+variable "environment" {
+}
